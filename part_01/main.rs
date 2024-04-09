e5cab5a15290e157df66502a6dfbb59f90ca5b7c
Mkps <alex.loubiere@gmail.com>
2024-04-09 15:32:28 +0200

    adding main.rs with print vec

---
+++ part_01/main.rs
@@ -0,0 +1,80 @@
+fn sqr(i: i32) -> i32 {
+    i * i
+}
+
+fn abs(i: i32) -> i32 {
+    if i >= 0   {i}
+    else        {-i}
+}
+
+enum NumberOrNothing {
+    Number(i32),
+    Nothing
+}
+use self::NumberOrNothing::{Number, Nothing};
+
+fn number_or_default(n: NumberOrNothing, default: i32) -> i32 
+{
+    match n {
+        Nothing => default,
+        Number(n) => n,
+    }
+}
+
+// returns the min of a vec32
+fn vec_min(v: Vec<i32>) ->NumberOrNothing {
+    fn min_i32(a: i32, b: i32) -> i32 {
+        if a > b    { a }
+        else        { b }
+    }
+    let mut min = Nothing;
+    for e in v {
+        min = Number(match min {
+        Nothing => e,
+        Number(n) => min_i32(n, e)
+        });
+    }
+    min
+}
+
+fn read_vec() -> Vec<i32> {
+    vec![18,5,7,2,9,27]
+}
+
+impl NumberOrNothing {
+    fn print(self) {
+        match self {
+            Nothing => println!("The number is: <nothing>"),
+            Number(n) =>println!("The number is: {}", n),
+        };
+    }
+}
+
+fn vec_print(v: Vec<i32>) {
+    for e in v {
+        println!("elem {}", e);
+    }
+}
+
+pub fn main()
+{
+    let i = 32;
+    let str = "hello";
+    println!("The number is: {}", i);
+    println!("The str is: {}", str);
+    let n = number_or_default(NumberOrNothing::Number(i), 42);
+    println!("The number as enum is: {}", n);
+    let n = number_or_default(NumberOrNothing::Nothing, 42);
+    println!("The str as enum is: {}", n);
+    let i = -2;
+    let n = sqr(i);
+    println!("The number i {} sqrd is: {}", i, n);
+    let i = -2;
+    let n = abs(i);
+    println!("The number i {} abs is: {}", i, n);
+    let vec = read_vec();
+    let min = vec_min(vec);
+    min.print();
+    let vec = read_vec();
+    vec_print(vec);
+}
