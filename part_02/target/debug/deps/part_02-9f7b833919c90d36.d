fda42b4333b7ea39963e66f8e4ec03a2192270e7
Mkps <alex.loubiere@gmail.com>
2024-04-09 18:07:33 +0200

    adding base

---
+++ part_02/target/debug/deps/part_02-9f7b833919c90d36.d
@@ -0,0 +1,5 @@
+/home/alx/Code/rust101/part_02/target/debug/deps/libpart_02-9f7b833919c90d36.rmeta: main.rs
+
+/home/alx/Code/rust101/part_02/target/debug/deps/part_02-9f7b833919c90d36.d: main.rs
+
+main.rs:
---
+++ part_02/target/debug/part_02.d
@@ -0,0 +1,1 @@
+/home/alx/Code/rust101/part_02/target/debug/part_02: /home/alx/Code/rust101/part_02/main.rs
